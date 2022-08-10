f342eaabd758c1122c08069c26278a0a7269832f
YuutoOse <y.ose@no4.co.jp>
2022-08-10 13:03:59 +0900

    chakra app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,85 @@
 import { useState } from "react";
+import {
+  Center,
+  HStack,
+  Box,
+  Button,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import type { ImageProps } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const MotionImage = motion<Omit<ImageProps, "transition">>(Image);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Center
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        margin: "0 auto",
+        padding: "2rem",
+        textAlign: "center",
+      }}
+    >
+      <Box>
+        <HStack justify="center">
+          <Link
+            as="a"
+            href="https://vitejs.dev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/vite.svg"
+              p={6}
+              sx={{ willChange: "filter", height: "6em" }}
+              _hover={{ filter: "drop-shadow(0 0 2em #646cffaa)" }}
+              alt="Vite logo"
+              m={0}
+            />
+          </Link>
+          <Link
+            as="a"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <MotionImage
+              as={motion.img}
+              animate={{ transform: ["rotate(0deg)", "rotate(360deg)"] }}
+              transition={{
+                repeat: Infinity,
+                duration: 20,
+                ease: "linear",
+              }}
+              src={reactLogo}
+              p={6}
+              sx={{ willChange: "filter", height: "6em" }}
+              _hover={{ filter: "drop-shadow(0 0 2em #646cffaa)" }}
+              alt="React logo"
+            />
+          </Link>
+        </HStack>
+        <Heading as="h1">Vite + React</Heading>
+        <Box p={8}>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <Text>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </Text>
+        </Box>
+        <Text sx={{ color: "#888" }}>
+          Click on the Vite and React logos to learn more
+        </Text>
+      </Box>
+    </Center>
   );
 }
 
